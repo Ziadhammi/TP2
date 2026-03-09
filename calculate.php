178f6d0b5756d67228aa2165022332aa942ae16e
Ziadhammi <8p5ygxg8x2@privaterelay.appleid.com>
2026-03-09 09:21:30 +0100

    Fix formatting and whitespace in calculate.php

--- calculate.php
+++ calculate.php
@@ -1,53 +1,53 @@
-<? php
-if ( isset ( $_POST [’course ’] , $_POST [’credits ’] , $_POST [’grade ’]) ) {
- $courses = $_POST [’course ’];
- $credits = $_POST [’credits ’];
- $grades = $_POST [’grade ’];
+<?php
+if ( isset ( $_POST['course'], $_POST['credits'], $_POST['grade'] ) ) {
+ $courses = $_POST['course'];
+ $credits = $_POST['credits'];
+ $grades = $_POST['grade'];
  $totalPoints = 0;
  $totalCredits = 0;
 
- echo "<table >";
- echo "<tr >
- <th > Course </th >
- <th > Credits </th >
- <th >Grade </th >
- <th > Grade Points </th >
- </tr >";
+ echo "<table>";
+ echo "<tr>
+ <th>Course</th>
+ <th>Credits</th>
+ <th>Grade</th>
+ <th>Grade Points</th>
+ </tr>";
 
  for ( $i = 0; $i < count ( $courses ) ; $i ++) {
- $course = htmlspecialchars ( $courses [ $i ]) ;
- $cr = floatval ( $credits [ $i ]) ;
- $g = floatval ( $grades [ $i ]) ;
+ $course = htmlspecialchars ( $courses [ $i ] ) ;
+ $cr = floatval ( $credits [ $i ] ) ;
+ $g = floatval ( $grades [ $i ] ) ;
  if ( $cr <= 0) continue ;
  $pts = $cr * $g ;
  $totalPoints += $pts ;
  $totalCredits += $cr ;
- echo "<tr >
- <td > $course </td >
- <td >$cr </td >
- <td >$g </td >
- <td >$pts </td >
- </tr >";
+ echo "<tr>
+ <td>$course</td>
+ <td>$cr</td>
+ <td>$g</td>
+ <td>$pts</td>
+ </tr>";
  }
- echo " </table >";
+ echo "</table>";
 
  if ( $totalCredits > 0) {
  $gpa = $totalPoints / $totalCredits ;
  if ( $gpa >= 3.7) {
- $interpretation = " Distinction ";
+ $interpretation = "Distinction";
  } elseif ( $gpa >= 3.0) {
- $interpretation = " Merit ";
+ $interpretation = "Merit";
  } elseif ( $gpa >= 2.0) {
- $interpretation = " Pass ";
+ $interpretation = "Pass";
  } else {
- $interpretation = " Fail ";
+ $interpretation = "Fail";
  }
- echo "<p> Your GPA is <strong >" . number_format ( $gpa , 2)
- . " </strong > ( $interpretation ). </p>";
+ echo "<p>Your GPA is <strong>" . number_format ( $gpa , 2)
+ . "</strong> ($interpretation).</p>";
  } else {
- echo "<p>No valid courses entered . </p>";
+ echo "<p>No valid courses entered.</p>";
  }
 } else {
-echo " Data not received .";
+echo "Data not received.";
  }
- ? >
+?>
